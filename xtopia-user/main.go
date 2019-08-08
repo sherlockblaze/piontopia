543f67e670f5a0ac5d955391340f9e05c52d015b
sherlockblaze <sherlockblaze@gmail.com>
2019-08-08 22:43:34 +0800

    init user service

--- xtopia-user/main.go
+++ xtopia-user/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/sherlockblaze/piontopia/xtopia-user/proto"
+	"github.com/sherlockblaze/piontopia/xtopia-user/service"
+	"google.golang.org/grpc"
+)
+
 func main() {
-	// do nothing now
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 3821))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("successfully listening on port: %d", 3821)
+
+	s := service.Server{}
+	grpcServer := grpc.NewServer()
+
+	proto.RegisterUserServicesServer(grpcServer, &s)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 }
---
+++ xtopia-user/service/userservice.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+	"log"
+
+	"github.com/sherlockblaze/piontopia/xtopia-user/proto"
+)
+
+// Server represents the gRPC server
+type Server struct {
+}
+
+// CreateUser create user
+func (s *Server) CreateUser(ctx context.Context, in *proto.User) (*proto.UserID, error) {
+	log.Printf("Receive message %s", in.Name)
+	return &proto.UserID{ID: "sher", Status: 0}, nil
+}
+
+// DeleteUser delete a user
+func (s *Server) DeleteUser(ctx context.Context, in *proto.UserID) (*proto.UserID, error) {
+	return nil, nil
+}
+
+// CheckoutUser checkout a user's info
+func (s *Server) CheckoutUser(ctx context.Context, in *proto.UserID) (*proto.User, error) {
+	return nil, nil
+}
